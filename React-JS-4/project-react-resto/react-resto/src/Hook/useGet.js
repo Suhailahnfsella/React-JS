7dd68baf0c91717b17eea673ed372849fccc3083
Suhailahnfsella <suhailahnfsella@gmail.com>
2021-03-13 22:16:59 +0700

    Tugas Minggu Ke-10 Smst4
    Video Pemrograman Web React-Js Ke 29-38

---
+++ React-JS-4/project-react-resto/react-resto/src/Hook/useGet.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { link } from "../Axios/link";
+
+const useGet = (url) => {
+ const [isi, setIsi] = useState([]);
+
+ //  useEffect(() => {
+ //   async function fetchData() {
+ //    const request = await link.get(url);
+ //    setIsi(request.data);
+ //   }
+ //   fetchData();
+ //  }, [isi]);
+
+ useEffect(() => {
+  let ambil = true;
+  async function fetchData() {
+   const res = await link.get(url);
+   if (ambil) {
+    setIsi(res.data);
+   }
+  }
+
+  fetchData();
+
+  return () => {
+   ambil = false;
+  };
+ }, [isi]);
+
+ return [isi];
+};
+
+export default useGet;
