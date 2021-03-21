b0b42b694febe78017f5ade96bae6c693290a1b3
Suhailahnfsella <suhailahnfsella@gmail.com>
2021-03-21 22:33:08 +0700

    Tugas Minggu Ke-11 Smst4
    Video Pemrograman Web React-JS Ke 39-46

---
+++ React-JS-5/project-react-resto/react-resto/src/Hook/useDelete.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { link } from "../Axios/link";
+
+const useDelete = (url) => {
+ const [pesan, setPesan] = useState("");
+
+ async function hapus(id) {
+  if (window.confirm("Yakin akan menghapus?")) {
+   const res = await link.delete(url + id);
+   setPesan(res.data.pesan);
+  }
+ }
+
+ return { hapus, pesan, setPesan };
+};
+
+export default useDelete;
