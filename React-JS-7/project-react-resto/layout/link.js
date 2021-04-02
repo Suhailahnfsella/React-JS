b384558b103a59f644a2747a191877039c9ac09c
Suhailahnfsella <68285716+Suhailahnfsella@users.noreply.github.com>
2021-04-02 18:49:04 +0700

    Delete React-JS-7/project-react-resto directory

--- React-JS-7/project-react-resto/layout/link.js
+++
@@ -1,9 +0,0 @@
-const url = "http://localhost:8000/api";
-let token = "dpycfkNyYR1KJlwFzC3TTchgB15W56RvHZuglkE4";
-
-export const link = axios.create({
-  baseURL: url,
-  headers: {
-    api_token: token,
-  },
-});
